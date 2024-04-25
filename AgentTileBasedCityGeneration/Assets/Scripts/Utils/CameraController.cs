055e9bea71dc8776f33b6f26d1f86efc4d977b0a
Flavia Brogle <flavia.brogle@fhnw.ch>
2024-04-25 12:37:38 +0200

    Make Camera work with IWorlds

--- AgentTileBasedCityGeneration/Assets/Scripts/Utils/CameraController.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/Utils/CameraController.cs
@@ -1,45 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
-using Simulation;
+using FreeFormGraph.World;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour {
+namespace Utils {
+    public class CameraController : MonoBehaviour {
     
-    private Camera Camera { get; set; }
-    [field: SerializeField] private float CameraSpeed { get; set; } = 10f;
-    [field: SerializeField] private float ZoomSpeed { get; set; } = 5f;
-    [field: SerializeField] private float ZoomSpeedMultiplicator { get; set; } = 10f;
-    [field: SerializeField] private float MinZoom { get; set; } = 5f;
-    [field: SerializeField] private float MaxZoom { get; set; } = 15f;
-    [field: SerializeField] private float AllowedBounds { get; set; } = 2f;
-    
-    private void Awake() {
-        Camera = GetComponent<Camera>();
-    }
+        private Camera Camera { get; set; }
+        [field: SerializeField] private float CameraSpeed { get; set; } = 10f;
+        [field: SerializeField] private float ZoomSpeed { get; set; } = 5f;
+        [field: SerializeField] private float ZoomSpeedMultiplicator { get; set; } = 10f;
+        [field: SerializeField] private float MinZoom { get; set; } = 5f;
+        [field: SerializeField] private float MaxZoom { get; set; } = 15f;
+        [field: SerializeField] private float AllowedBounds { get; set; } = 2f;
 
-    private void Update() {
-        var horizontal = Input.GetAxis("Horizontal");
-        var vertical = Input.GetAxis("Vertical");
-        var zoom = Input.GetAxis("Mouse ScrollWheel");
-        var ctrl = Input.GetKey(KeyCode.LeftControl);
-        
-        var size = Camera.orthographicSize;
-        size -= zoom * ZoomSpeed * (ctrl ? ZoomSpeedMultiplicator : 1);
-        size = Mathf.Clamp(size, MinZoom, MaxZoom);
-        Camera.orthographicSize = size;
-        
-        var position = transform.position;
-        position.x += horizontal * CameraSpeed * Time.deltaTime * size / 10f;
-        position.y += vertical * CameraSpeed * Time.deltaTime * size / 10f;
-        var minX = Camera.orthographicSize * Camera.aspect - 0.5f - AllowedBounds;
-        var minY = Camera.orthographicSize - 0.5f - AllowedBounds;
-        var maxX = World.Instance.Width - minX;
-        var maxY = World.Instance.Height - minY;
-        position.x = Mathf.Clamp(position.x, minX, maxX);
-        position.y = Mathf.Clamp(position.y, minY, maxY);
-        transform.position = position;
-        
-        
-    }
+        private WorldGameObject world;
     
+        private void Awake() {
+            Camera = GetComponent<Camera>();
+            world = FindObjectOfType<WorldGameObject>();
+        }
+
+        private void Update() {
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+            var zoom = Input.GetAxis("Mouse ScrollWheel");
+            var ctrl = Input.GetKey(KeyCode.LeftControl);
+        
+            var size = Camera.orthographicSize;
+            size -= zoom * ZoomSpeed * (ctrl ? ZoomSpeedMultiplicator : 1);
+            size = Mathf.Clamp(size, MinZoom, MaxZoom);
+            Camera.orthographicSize = size;
+        
+            var position = transform.position;
+            position.x += horizontal * CameraSpeed * Time.deltaTime * size / 10f;
+            position.y += vertical * CameraSpeed * Time.deltaTime * size / 10f;
+            var minX = Camera.orthographicSize * Camera.aspect - 0.5f - AllowedBounds;
+            var minY = Camera.orthographicSize - 0.5f - AllowedBounds;
+            var maxX = world.Width - minX;
+            var maxY = world.Height - minY;
+            position.x = Mathf.Clamp(position.x, minX, maxX);
+            position.y = Mathf.Clamp(position.y, minY, maxY);
+            transform.position = position;
+        
+        
+        }
+    
+    }
 }
