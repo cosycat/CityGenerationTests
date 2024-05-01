6bedcdd159751ce1ed644581b6d557424b583481
Toni Lozancic <toni.lozancic@students.fhnw.ch>
2024-05-01 22:00:27 +0200

    make world renderer colors changable (should have been in ebf1f09f16d457d8432bcc37482a3d312d03dbd0)

--- AgentTileBasedCityGeneration/Assets/Scripts/FreeFormGraph/World/WorldRenderer.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/FreeFormGraph/World/WorldRenderer.cs
@@ -53,8 +53,6 @@ namespace FreeFormGraph.World {
             var triangles = new int[(width - 1) * (height - 1) * 6];
             var triangleIndex = 0;
 
-            var startColor = new Color(0.91f, 0.83f, 0.02f, 1f);
-            var endColor = new Color(0.91f, 0, 0, 1f);
 
             int worldX = startX;
             int worldY = startY;
