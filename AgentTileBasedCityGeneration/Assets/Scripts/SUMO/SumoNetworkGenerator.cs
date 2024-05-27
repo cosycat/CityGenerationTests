cfcfc36ce91553b11d54e8de5e3bcf01047f27a8
Flavia Brogle <flavia.brogle@fhnw.ch>
2024-05-27 14:34:00 +0200

    Create Routes and Configuration files and extract file creation to separate class

--- AgentTileBasedCityGeneration/Assets/Scripts/SUMO/SumoNetworkGenerator.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/SUMO/SumoNetworkGenerator.cs
@@ -1,10 +1,8 @@
-using System;
 using System.Collections;
 using System.Diagnostics;
 using System.Linq;
 using System.Xml.Linq;
 using FreeFormGraph;
-using FreeFormGraph.LineBased;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
@@ -14,145 +12,30 @@ namespace SUMO {
     /// Generates a SUMO network from a given graph.
     /// </summary>
     public class SumoNetworkGenerator : MonoBehaviour {
-        private const string NETCONVERT_PATH_HOMEBREW = "/opt/homebrew/bin/netconvert"; // TODO Add more systems and installations. This only works on macOS with Homebrew installation of SUMO.
-        private static string SumoPath => $"{Application.persistentDataPath}/SUMO";
-        private string NodesFilePath => $"{SumoPath}/nodes.nod.xml";
-        private string EdgesFilePath => $"{SumoPath}/edges.edg.xml";
-        private string ConnectionsFilePath => $"{SumoPath}/connections.con.xml";
-        private string OutputNetFilePath => $"{SumoPath}/output.net.xml";
+        private const string NETCONVERT_PATH_HOMEBREW = "/opt/homebrew/bin/netconvert"; // TODO Add more systems and installations. This only works on macOS with Homebrew installation of SUMO. alternatives: "/usr/local/bin/netconvert"
+        
+        [SerializeField] private bool openFolderAfterGeneration = true;
+        
+        private string SumoPath { get; set; }
 
-        private void Start() {
-            GenerateTestGraph();
-        }
-
-        private void GenerateTestGraph() {
-            var graph = new GameObject("TestGraph").AddComponent<LineGraph>();
-            graph.CreateUnconnectedNode(new Vector3(0, 0, 0), out var n0);
-            
-            graph.CreateEdge(n0, new Vector3(100, 0, 0), out var e0, out var n1, out var isToNodeNew, out var isEdgeNew);
-            graph.CreateEdge(n1, new Vector3(100, 100, 0), out var e1, out var n2, out var isToNodeNew1, out var isEdgeNew1);
-            graph.CreateEdge(n2, new Vector3(0, 100, 0), out var e2, out var n3, out var isToNodeNew2, out var isEdgeNew2);
-            graph.CreateEdge(n3, new Vector3(0, 0, 0), out var e3, out var n02, out var isToNodeNew3, out var isEdgeNew3);
-            Debug.Assert(n0 == n02, "n0 == n02");
-            Debug.Assert(!isToNodeNew3);
-            Debug.Assert(isEdgeNew3); 
-
-            GenerateNetwork(graph);
-            StartCoroutine(ConvertToSumoNetwork(NodesFilePath, EdgesFilePath, ConnectionsFilePath, OutputNetFilePath));
+        private void Awake() {
+            SumoPath = $"{Application.persistentDataPath}/SUMO";
         }
 
         public void GenerateNetwork(IStreetGraph graph) {
             Debug.Log($"Generating SUMO network from graph with {graph.NodeCount} nodes and {graph.EdgeCount} edges in {SumoPath}..");
-            InitializeFiles();
-            GenerateNodes(NodesFilePath, graph);
-            GenerateEdges(EdgesFilePath, graph);
-            GenerateConnections(ConnectionsFilePath, graph);
+            var sumoFileGenerator = SumoFileGenerator.Create(SumoPath, graph);
+            StartCoroutine(ConvertToSumoNetwork(sumoFileGenerator));
         }
 
-        private void InitializeFiles() {
-            if (!System.IO.Directory.Exists(SumoPath)) {
-                System.IO.Directory.CreateDirectory(SumoPath);
-            }
-            if (System.IO.File.Exists(NodesFilePath)) {
-                System.IO.File.Delete(NodesFilePath);
-            }
-            if (System.IO.File.Exists(EdgesFilePath)) {
-                System.IO.File.Delete(EdgesFilePath);
-            }
-            if (System.IO.File.Exists(ConnectionsFilePath)) {
-                System.IO.File.Delete(ConnectionsFilePath);
-            }
-            if (System.IO.File.Exists(OutputNetFilePath)) {
-                System.IO.File.Delete(OutputNetFilePath);
-            }
-            // create output file
-            // System.IO.File.Create(OutputNetFilePath);
-        }
-
-        private static void GenerateNodes(string nodesFilePath, IStreetGraph graph) {
-            var nodesDoc = new XDocument(new XElement("nodes"));
+        private IEnumerator ConvertToSumoNetwork(SumoFileGenerator sumoFileGenerator) {
+            var nodesFilePath = sumoFileGenerator.NodesFilePath;
+            var edgesFilePath = sumoFileGenerator.EdgesFilePath;
+            var connectionsFilePath = sumoFileGenerator.ConnectionsFilePath;
+            var routesFilePath = sumoFileGenerator.RoutesFilePath;
+            var outputNetFilePath = sumoFileGenerator.OutputNetFilePath;
+            var configurationFilePath = sumoFileGenerator.ConfigurationFilePath;
             
-            var nodes = graph.Nodes.ToArray();
-            for (var i = 0; i < nodes.Length; i++) {
-                var node = nodes[i];
-                AddNode(nodesDoc, $"n{i}", node.Position.x, node.Position.y, "priority");
-            }
-            
-            nodesDoc.Save(nodesFilePath);
-        }
-
-        private static void AddNode(XDocument nodesDoc, string nodeId, double x, double y, string type) {
-            var newNode = new XElement("node",
-                new XAttribute("id", nodeId),
-                new XAttribute("x", x),
-                new XAttribute("y", y),
-                new XAttribute("type", type)
-            );
-
-            Debug.Assert(nodesDoc.Root != null, "nodesDoc.Root != null");
-            nodesDoc.Root.Add(newNode);
-        }
-
-        private static void GenerateEdges(string edgesFilePath, IStreetGraph graph) {
-            var edgesDoc = new XDocument(new XElement("edges"));
-            
-            var edges = graph.Edges.ToArray();
-            for (var i = 0; i < edges.Length; i++) {
-                var edge = edges[i];
-                var nodeA = edge.NodeA;
-                var nodeB = edge.NodeB;
-                var nodeAIndex = Array.IndexOf(graph.Nodes.ToArray(), nodeA);
-                var nodeBIndex = Array.IndexOf(graph.Nodes.ToArray(), nodeB);
-                AddEdge(edgesDoc, $"e{i}", $"n{nodeAIndex}", $"n{nodeBIndex}", 1, 1, 10);
-                AddEdge(edgesDoc, $"e{i}_reverse", $"n{nodeBIndex}", $"n{nodeAIndex}", 1, 1, 10);
-            }
-            
-            edgesDoc.Save(edgesFilePath);
-        }
-
-        private static void AddEdge(XDocument edgesDoc, string edgeId, string fromNode, string toNode, int priority, int numLanes, double speed) {
-            var newEdge = new XElement("edge",
-                new XAttribute("id", edgeId),
-                new XAttribute("from", fromNode),
-                new XAttribute("to", toNode),
-                new XAttribute("priority", priority),
-                new XAttribute("numLanes", numLanes),
-                new XAttribute("speed", speed)
-            );
-
-            Debug.Assert(edgesDoc.Root != null, "edgesDoc.Root != null");
-            edgesDoc.Root.Add(newEdge);
-        }
-
-        private static void GenerateConnections(string connectionsFilePath, IStreetGraph graph)
-        {
-            var connectionsDoc = new XDocument(new XElement("connections"));
-
-            // // Define connections (optional)
-            // AddConnection(connectionsDoc, "e1", "e2", "1", "1");
-            // AddConnection(connectionsDoc, "e2", "e3", "1", "1");
-            // AddConnection(connectionsDoc, "e3", "e4", "1", "1");
-            // AddConnection(connectionsDoc, "e4", "e1", "1", "1");
-
-            connectionsDoc.Save(connectionsFilePath);
-        }
-
-        private static void AddConnection(XDocument connectionsDoc, string fromEdge, string toEdge, string fromLane, string toLane)
-        {
-            var newConnection = new XElement("connection",
-                new XAttribute("from", fromEdge),
-                new XAttribute("to", toEdge),
-                new XAttribute("fromLane", fromLane),
-                new XAttribute("toLane", toLane)
-            );
-
-            Debug.Assert(connectionsDoc.Root != null, "connectionsDoc.Root != null");
-            connectionsDoc.Root.Add(newConnection);
-        }
-
-        private IEnumerator ConvertToSumoNetwork(string nodesFilePath, string edgesFilePath, string connectionsFilePath,
-            string outputNetFilePath) {
-            // const string netconvertPath = "/usr/local/bin/netconvert"; // Ensure netconvert is installed and path is correct
             var arguments = $"--node-files=\"{nodesFilePath}\" --edge-files=\"{edgesFilePath}\" --connection-files=\"{connectionsFilePath}\" --output-file=\"{outputNetFilePath}\"";
 
             var startInfo = new ProcessStartInfo {
@@ -166,8 +49,8 @@ namespace SUMO {
 
             using (var process = new Process()) {
                 process.StartInfo = startInfo;
-                process.OutputDataReceived += (sender, e) => Debug.Log(e.Data);
-                process.ErrorDataReceived += (sender, e) => Debug.LogError(e.Data);
+                process.OutputDataReceived += (_, e) => Debug.Log(e.Data);
+                process.ErrorDataReceived += (_, e) => Debug.LogError(e.Data);
 
                 process.Start();
                 process.BeginOutputReadLine();
@@ -184,37 +67,226 @@ namespace SUMO {
                     Debug.Log("netconvert completed successfully.");
                 }
             }
+            
+            if (openFolderAfterGeneration) {
+                using var process = new Process();
+                process.StartInfo.FileName = "open";
+                process.StartInfo.Arguments = $"\"{SumoPath}\"";
+
+                process.Start();
+
+                while (!process.HasExited) {
+                    yield return null;
+                }
+                    
+                if (process.ExitCode != 0) {
+                    Debug.LogError($"Failed to open folder with exit code {process.ExitCode}");
+                }
+                else {
+                    Debug.Log($"Opened folder {SumoPath}.");
+                }
+            }
 
             Debug.Log("Conversion completed.");
         }
+        
+    }
 
-        // private static void ConvertToSumoNetwork(string nodesFilePath, string edgesFilePath, string connectionsFilePath, string outputNetFilePath)
-        // {
-        //     var startInfo = new ProcessStartInfo
-        //     {
-        //         FileName = "netconvert",
-        //         Arguments = $"--node-files={nodesFilePath} --edge-files={edgesFilePath} --output-file={outputNetFilePath}",
-        //         RedirectStandardOutput = true,
-        //         RedirectStandardError = true,
-        //         UseShellExecute = false,
-        //         CreateNoWindow = true
-        //     };
-        //
-        //     using (var process = Process.Start(startInfo))
-        //     {
-        //         if (process == null) {
-        //             Debug.Log("Failed to start conversion process");
-        //             return;
-        //         }
-        //
-        //         process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
-        //         process.ErrorDataReceived += (sender, e) => Console.WriteLine(e.Data);
-        //
-        //         process.BeginOutputReadLine();
-        //         process.BeginErrorReadLine();
-        //
-        //         process.WaitForExit();
-        //     }
-        // }
+    internal class SumoFileGenerator {
+        private const string EDGES_FILE_NAME = "edges.edg.xml";
+        private const string NODES_FILE_NAME = "nodes.nod.xml";
+        private const string CONNECTIONS_FILE_NAME = "connections.con.xml";
+        private const string ROUTES_FILE_NAME = "routes.rou.xml";
+        private const string OUTPUT_NET_FILE_NAME = "output.net.xml";
+        private const string CONFIGURATION_FILE_NAME = "configuration.sumocfg";
+
+        private IStreetGraph Graph { get; }
+        private string SumoPath { get; }
+
+        public string NodesFilePath => $"{SumoPath}/{NODES_FILE_NAME}";
+        public string EdgesFilePath => $"{SumoPath}/{EDGES_FILE_NAME}";
+        public string ConnectionsFilePath => $"{SumoPath}/{CONNECTIONS_FILE_NAME}";
+        public string RoutesFilePath => $"{SumoPath}/{ROUTES_FILE_NAME}";
+        public string OutputNetFilePath => $"{SumoPath}/{OUTPUT_NET_FILE_NAME}";
+        public string ConfigurationFilePath => $"{SumoPath}/{CONFIGURATION_FILE_NAME}";
+
+        private SumoFileGenerator(string sumoPath, IStreetGraph graph) {
+            Graph = graph;
+            SumoPath = sumoPath;
+            InitializeDirectory(sumoPath);
+            CreateNetworkFiles();
+        }
+        
+        internal static SumoFileGenerator Create(string sumoPath, IStreetGraph graph) {
+            return new SumoFileGenerator(sumoPath, graph);
+        }
+
+        private static void InitializeDirectory(string sumoPath) {
+            if (!System.IO.Directory.Exists(sumoPath)) {
+                System.IO.Directory.CreateDirectory(sumoPath);
+                return;
+            }
+
+            var directoryInfo = new System.IO.DirectoryInfo(sumoPath);
+            foreach (var file in directoryInfo.GetFiles()) {
+                file.Delete();
+            }
+                
+            foreach (var dir in directoryInfo.GetDirectories()) {
+                dir.Delete(true);
+            }
+        }
+        
+        private void CreateNetworkFiles() {
+            GenerateNodes();
+            GenerateEdges();
+            GenerateConnections();
+            GenerateRoutes();
+            GenerateConfiguration();
+        }
+        
+        
+        private void GenerateNodes() {
+            var nodesDoc = new XDocument(new XElement("nodes"));
+            
+            var nodes = Graph.Nodes.ToArray();
+            for (var i = 0; i < nodes.Length; i++) {
+                var node = nodes[i];
+                AddNode(nodesDoc, $"n{i}", node.Position.x, node.Position.y, "priority");
+            }
+            
+            nodesDoc.Save(NodesFilePath);
+        }
+
+        private static void AddNode(XDocument nodesDoc, string nodeId, double x, double y, string type) {
+            var newNode = new XElement("node",
+                new XAttribute("id", nodeId),
+                new XAttribute("x", x),
+                new XAttribute("y", y),
+                new XAttribute("type", type)
+            );
+
+            Debug.Assert(nodesDoc.Root != null, "nodesDoc.Root != null");
+            nodesDoc.Root.Add(newNode);
+        }
+
+        private void GenerateEdges() {
+            var edgesDoc = new XDocument(new XElement("edges"));
+            
+            var edges = Graph.Edges.ToArray();
+            var nodes = Graph.Nodes.Select((node, i) => (node, i)).ToDictionary(t => t.node, t => t.i);
+            for (var i = 0; i < edges.Length; i++) {
+                var edge = edges[i];
+                var nodeA = edge.NodeA;
+                var nodeB = edge.NodeB;
+                var nodeAIndex = nodes[nodeA];
+                var nodeBIndex = nodes[nodeB];
+                AddEdge(edgesDoc, $"e{i}", $"n{nodeAIndex}", $"n{nodeBIndex}", 1, 1, 10);
+                AddEdge(edgesDoc, $"e{i}_reverse", $"n{nodeBIndex}", $"n{nodeAIndex}", 1, 1, 10);
+            }
+            
+            edgesDoc.Save(EdgesFilePath);
+        }
+
+        private static void AddEdge(XDocument edgesDoc, string edgeId, string fromNode, string toNode, int priority, int numLanes, double speed) {
+            var newEdge = new XElement("edge",
+                new XAttribute("id", edgeId),
+                new XAttribute("from", fromNode),
+                new XAttribute("to", toNode),
+                new XAttribute("priority", priority),
+                new XAttribute("numLanes", numLanes),
+                new XAttribute("speed", speed)
+            );
+
+            Debug.Assert(edgesDoc.Root != null, "edgesDoc.Root != null");
+            edgesDoc.Root.Add(newEdge);
+        }
+
+        private void GenerateConnections()
+        {
+            var connectionsDoc = new XDocument(new XElement("connections"));
+            
+            var edges = Graph.Edges.Select((edge, i) => (edge, i)).ToDictionary(t => t.edge, t => t.i);
+            var nodes = Graph.Nodes.ToArray();
+            
+            foreach (var node in nodes) {
+                var connectedEdges = node.Edges.ToArray();
+                foreach (var edgeJ in connectedEdges) {
+                    var edgeJIndex = edges[edgeJ];
+                    foreach (var edgeK in connectedEdges) {
+                        var edgeKIndex = edges[edgeK];
+                        if (edgeJ == edgeK) continue;
+                        
+                        var fromEdge = $"e{edgeJIndex}" + (edgeJ.NodeB == node ? "" : "_reverse"); // the fromEdge needs to be the edge going into the node, while the toEdge needs to be the edge going out of the node
+                        var toEdge = $"e{edgeKIndex}" + (edgeK.NodeA == node ? "" : "_reverse");
+                        
+                        AddConnection(connectionsDoc, fromEdge, toEdge, "0", "0");
+                    }
+                }
+            }
+
+            connectionsDoc.Save(ConnectionsFilePath);
+        }
+
+        private static void AddConnection(XDocument connectionsDoc, string fromEdge, string toEdge, string fromLane, string toLane)
+        {
+            var newConnection = new XElement("connection",
+                new XAttribute("from", fromEdge),
+                new XAttribute("to", toEdge),
+                new XAttribute("fromLane", fromLane),
+                new XAttribute("toLane", toLane)
+            );
+
+            Debug.Assert(connectionsDoc.Root != null, "connectionsDoc.Root != null");
+            connectionsDoc.Root.Add(newConnection);
+        }
+
+        private void GenerateRoutes() {
+            var routesDoc = new XDocument(new XElement("routes"));
+
+            // example route
+            var vType = new XElement("vType",
+                new XAttribute("accel", 1.0),
+                new XAttribute("decel", 5.0),
+                new XAttribute("id", "Car"),
+                new XAttribute("length", 2.0),
+                new XAttribute("maxSpeed", 15.0),
+                new XAttribute("sigma", 0.0)
+            );
+            routesDoc.Root?.Add(vType);
+            
+            var route = new XElement("route",
+                new XAttribute("id", "route0"),
+                new XAttribute("edges", "e0 e1 e2 e3")
+            );
+            routesDoc.Root?.Add(route);
+            
+            var vehicle = new XElement("vehicle",
+                new XAttribute("depart", 1),
+                new XAttribute("id", "veh0"),
+                new XAttribute("route", "route0"),
+                new XAttribute("type", "Car")
+            );
+            routesDoc.Root?.Add(vehicle);
+            
+            routesDoc.Save(RoutesFilePath);
+        }
+
+        private void GenerateConfiguration() {
+            var configurationDoc = new XDocument(new XElement("configuration"));
+            var input = new XElement("input",
+                new XElement("net-file", new XAttribute("value", OUTPUT_NET_FILE_NAME)),
+                new XElement("route-files", new XAttribute("value", ROUTES_FILE_NAME))
+            );
+            configurationDoc.Root?.Add(input);
+            
+            var time = new XElement("time",
+                new XElement("begin", new XAttribute("value", "0")),
+                new XElement("end", new XAttribute("value", "10000"))
+            );
+            configurationDoc.Root?.Add(time);
+            
+            configurationDoc.Save(ConfigurationFilePath);
+        }
     }
 }
