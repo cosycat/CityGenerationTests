50b2d713c935b9a2577f1d7635d55a47ea321699
Flavia Brogle <flavia.brogle@fhnw.ch>
2024-05-27 16:44:32 +0200

    Add H-shaped graph

--- AgentTileBasedCityGeneration/Assets/Scripts/FreeFormGraph/LineBased/LineGraphTestCreator.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/FreeFormGraph/LineBased/LineGraphTestCreator.cs
@@ -19,6 +19,24 @@ namespace FreeFormGraph.LineBased {
             
             return graph;
         }
+
+        public static LineGraph GenerateHShapedGraph() {
+            var graph = new GameObject("TestGraph").AddComponent<LineGraph>();
+            graph.CreateUnconnectedNode(new Vector3(0, 0, 0), out var nLeftBottom);
+            
+            // Left Side
+            graph.CreateEdge(nLeftBottom, new Vector3(0, 100, 0), out var e0, out var nLeftMiddle, out var isToNodeNew, out var isEdgeNew);
+            graph.CreateEdge(nLeftMiddle, new Vector3(0, 200, 0), out var e1, out var nLeftTop, out var isToNodeNew1, out var isEdgeNew1);
+            
+            // Middle
+            graph.CreateEdge(nLeftMiddle, new Vector3(100, 100, 0), out var e2, out var nRightMiddle, out var isToNodeNew2, out var isEdgeNew2);
+            
+            // Right Side
+            graph.CreateEdge(nRightMiddle, new Vector3(100, 200, 0), out var e3, out var nRightTop, out var isToNodeNew3, out var isEdgeNew3);
+            graph.CreateEdge(nRightMiddle, new Vector3(100, 0, 0), out var e4, out var nRightBottom, out var isToNodeNew4, out var isEdgeNew4);
+            
+            return graph;
+        }
         
     }
 }
