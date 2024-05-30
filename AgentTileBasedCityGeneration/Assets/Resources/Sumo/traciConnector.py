dabce16bc76edc1465ada63b7de1e195e7313e30
Flavia Brogle <flavia.brogle@fhnw.ch>
2024-05-31 01:05:54 +0200

    traciConnector.py loops over all vehicles, sends them as string to unity

--- AgentTileBasedCityGeneration/Assets/Resources/Sumo/traciConnector.py
+++ AgentTileBasedCityGeneration/Assets/Resources/Sumo/traciConnector.py
@@ -3,16 +3,55 @@ import time
 import traci.constants as tc
 import socket
 import os
- 
-# Function to add a vehicle to the simulation
+
+# documentation at https://sumo.dlr.de/pydoc/traci.html
+
+IS_DEBUG = False
+
+should_stop = False
+
+### Connection Methods ###
+
+def send_data_over_socket(data, conn):
+    print(f"Sending data: {data}")
+    if IS_DEBUG:
+        return 
+    conn.sendall(data.encode('utf-8'))
+        
+def start_socket_server():
+    if IS_DEBUG:
+        print("Starting socket server...")
+        return None, None
+    # Setup socket server
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', 9999))
+    server_socket.listen(1)
+    
+    print("Waiting for a connection from Unity...")
+    conn, addr = server_socket.accept()
+    print("Connected by", addr)
+    
+    return conn, addr
+
+def receive_data_over_socket(conn):
+    if IS_DEBUG:
+        return
+    data = conn.recv(1024)
+    if data.decode('utf-8') == "stop":
+        should_stop = True
+
+
+### Additional TraCI Methods ###
+
 def add_vehicle(vehicle_id, route_id):
     traci.vehicle.add(vehicle_id, route_id)
     traci.vehicle.setSpeedMode(vehicle_id, 0)  # Disable automatic speed regulation
- 
-# Function to manually control the vehicle's steering
+
 def move_vehicle(vehicle_id, x, y):
     traci.vehicle.moveToXY(vehicle_id, "", 0, x, y, angle=0, keepRoute=2)
     
+        
+### Main Loop ###
  
 def run_sumo_simulation():
     # Setup SUMO
@@ -24,23 +63,17 @@ def run_sumo_simulation():
 #    traci.connect(port=57230)
     
     # Setup socket server
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 9999))
-    server_socket.listen(1)
-    
-    print("Waiting for a connection from Unity...")
-    conn, addr = server_socket.accept()
-    print("Connected by", addr)
+    conn, addr = start_socket_server()
      
-    vehicle_id = "vehicle_0"
-    route_id = "1_traffic"  # Ensure this route exists in your SUMO config
-    add_vehicle(vehicle_id, route_id)
+    # vehicle_id = "vehicle_0"
+    # route_id = "1_traffic"  # Ensure this route exists in your SUMO config
+    # add_vehicle(vehicle_id, route_id)
      
     # Simulation loop
     step = 0
     x = 5
     y = 2
-    while traci.simulation.getMinExpectedNumber() > 0:
+    while traci.simulation.getMinExpectedNumber() > 0 and not should_stop:
         traci.simulationStep()
      
 #        x += 1
@@ -53,17 +86,16 @@ def run_sumo_simulation():
 #            position = traci.vehicle.getPosition(vehicle_id)
 #            print(f"Vehicle {vehicle_id}: Position {position}")
 
-        speed = traci.vehicle.getSpeed(vehicle_id)
-        position = traci.vehicle.getPosition(vehicle_id)
-        conn.sendall(f"{vehicle_id};{speed};{position}".encode('utf-8'))
+        positions = []
+        for vehicle_id in traci.vehicle.getIDList():
+            position = traci.vehicle.getPosition(vehicle_id)
+            positions.append((vehicle_id, position))
         
-        data = conn.recv(1024)
-        if data.decode('utf-8') == "stop":
-            break
+        send_data_over_socket(str(positions), conn)
+        receive_data_over_socket(conn)
      
-        # Your simulation logic here
         step += 1
-        #time.sleep(1)
+        # time.sleep(1)
         
      
     # Close TraCI connection
