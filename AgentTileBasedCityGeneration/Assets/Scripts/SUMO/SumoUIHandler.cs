aa0dd3067bc1355180fd98c235b14fc0d6545a7a
Flavia Brogle <flavia.brogle@fhnw.ch>
2024-05-31 01:06:37 +0200

    SumiUIHandler can start Connection

--- AgentTileBasedCityGeneration/Assets/Scripts/SUMO/SumoUIHandler.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/SUMO/SumoUIHandler.cs
@@ -7,6 +7,10 @@ namespace SUMO {
     [RequireComponent(typeof(SumoNetworkGenerator))]
     public class SumoUIHandler : MonoBehaviour {
         
+        [SerializeField] private bool runSimulationAfterGeneration = false;
+        [SerializeField] private bool openFolderAfterGeneration = true;
+        [SerializeField] private bool openSumoGUIAfterGeneration = false;
+        
         private SumoNetworkGenerator networkGenerator;
         private IStreetGraph graph;
         
@@ -19,12 +23,34 @@ namespace SUMO {
             GUILayout.BeginArea(new Rect(10, 500, 150, 100));
             if (GUILayout.Button("Convert to Sumo")) {
                 AgentManager.Instance.RequestStopAgents(() => {
-                    networkGenerator.GenerateNetwork(graph, true, true, true, () => {
-                        Debug.Log("Done generating SUMO network.");
-                        AgentManager.Instance.RestartAgents();
-                    });
+                    networkGenerator.GenerateNetwork(graph, 
+                        convertToSumoNetwork: true,
+                        runSimulationAfterGeneration: runSimulationAfterGeneration, 
+                        openFolderAfterGeneration: openFolderAfterGeneration, 
+                        openSumoGUIAfterGeneration: openSumoGUIAfterGeneration,
+                        onDone: () => {
+                            Debug.Log("Done generating SUMO network.");
+                            // AgentManager.Instance.RestartAgents();
+                        });
                 });
             }
+
+            if (!networkGenerator.IsNetworkGenerated) {
+                GUILayout.EndArea();
+                return;
+            }
+
+            if (!networkGenerator.IsSimulationRunning) {
+                if (GUILayout.Button("Connect Simulation")) {
+                    networkGenerator.StartClient();
+                }
+            }
+            else {
+                if (GUILayout.Button("Stop Simulation")) {
+                    networkGenerator.RequestStopSimulation();
+                }
+            }
+            
             GUILayout.EndArea();
         }
     }
