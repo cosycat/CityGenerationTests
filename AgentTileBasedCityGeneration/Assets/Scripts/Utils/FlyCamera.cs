713a9b298c28d33a862138351f4377282180b277
Flavia Brogle <flavia.brogle@fhnw.ch>
2024-05-31 15:20:50 +0200

    Enable locking the mouse

--- AgentTileBasedCityGeneration/Assets/Scripts/Utils/FlyCamera.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/Utils/FlyCamera.cs
@@ -14,6 +14,7 @@ namespace Utils {
     shift : Makes camera accelerate
     space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
      
+        private bool MouseLookEnabled { get; set; } = true;
      
         float mainSpeed = 100.0f; //regular speed
         float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
@@ -23,38 +24,44 @@ namespace Utils {
         private float totalRun= 1.0f;
      
         void Update () {
-            lastMouse = Input.mousePosition - lastMouse ;
-            lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
-            lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
-            transform.eulerAngles = lastMouse;
-            lastMouse =  Input.mousePosition;
-            //Mouse  camera angle done.  
-       
+            if (Input.GetKeyDown(KeyCode.Tab)) {
+                MouseLookEnabled = !MouseLookEnabled;
+            }
+            
+            if (MouseLookEnabled) {
+                lastMouse = Input.mousePosition - lastMouse;
+                lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0);
+                lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x, transform.eulerAngles.y + lastMouse.y,
+                    0);
+                transform.eulerAngles = lastMouse;
+                lastMouse = Input.mousePosition;
+                //Mouse  camera angle done.  
+            }
+
             //Keyboard commands
-            float f = 0.0f;
-            Vector3 p = GetBaseInput();
-            if (p.sqrMagnitude > 0){ // only move while a direction key is pressed
-                if (Input.GetKey (KeyCode.LeftShift)){
-                    totalRun += Time.deltaTime;
-                    p  = p * totalRun * shiftAdd;
-                    p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
-                    p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
-                    p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
-                } else {
-                    totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
-                    p = p * mainSpeed;
-                }
+            // float f = 0.0f;
+            var p = GetBaseInput();
+            if (p.sqrMagnitude <= 0) return; // only move while a direction key is pressed
+            if (Input.GetKey (KeyCode.LeftShift)){
+                totalRun += Time.deltaTime;
+                p  = p * totalRun * shiftAdd;
+                p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+                p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+                p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+            } else {
+                totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+                p = p * mainSpeed;
+            }
          
-                p = p * Time.deltaTime;
-                Vector3 newPosition = transform.position;
-                if (Input.GetKey(KeyCode.Space)){ //If player wants to move on X and Z axis only
-                    transform.Translate(p);
-                    newPosition.x = transform.position.x;
-                    newPosition.z = transform.position.z;
-                    transform.position = newPosition;
-                } else {
-                    transform.Translate(p);
-                }
+            p = p * Time.deltaTime;
+            Vector3 newPosition = transform.position;
+            if (Input.GetKey(KeyCode.Space)){ //If player wants to move on X and Z axis only
+                transform.Translate(p);
+                newPosition.x = transform.position.x;
+                newPosition.z = transform.position.z;
+                transform.position = newPosition;
+            } else {
+                transform.Translate(p);
             }
         }
      
