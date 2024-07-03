ec677637683ec4ce6057c3cdd78c713c17d91217
Toni Lozancic <toni.lozancic@students.fhnw.ch>
2024-07-03 20:11:45 +0200

    2d world renderer: use a texture instead of rendering multiple meshes (why even use meshes for display a texture?)

--- AgentTileBasedCityGeneration/Assets/Scripts/FreeFormGraph/Visualisation/WorldRenderer2D.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/FreeFormGraph/Visualisation/WorldRenderer2D.cs
@@ -2,11 +2,10 @@ using UnityEngine;
 using FreeFormGraph.World;
 
 namespace FreeFormGraph.Visualisation {
-    public class WorldRenderer2D: MonoBehaviour {
+    public class WorldRenderer2D : MonoBehaviour {
         public IWorld World;
         [SerializeField]
         public Material material;
-        private bool didCreateMesh = false;
 
         [SerializeField] private Color[] colors = {
             Color.blue,
@@ -16,8 +15,9 @@ namespace FreeFormGraph.Visualisation {
         };
 
         private Gradient colorGradient;
+        private Texture2D texture;
 
-        private void Start() {
+        private void Awake() {
             colorGradient = new Gradient();
             if (colors == null || colors.Length < 2) {
                 colors = new Color[2];
@@ -34,94 +34,36 @@ namespace FreeFormGraph.Visualisation {
             colorGradient.SetKeys(colorGradients, alphas);
         }
 
-        private void Update() {
-            if(World != null) {
-                if(!didCreateMesh) {
-                    RenderSubmaps();
-                    didCreateMesh = true; //ugh
-                }
-            } else {
-                World = FindObjectOfType<WorldGameObject>();
-            }
+        private void Start() {
+            World = FindObjectOfType<WorldGameObject>();
+            RenderTexture();
         }
 
-        void RenderSubmaps() {
-            int meshSize = 200;
-            for(int x = 0; x < World.Width; x+=meshSize) {
-                for(int y = 0; y < World.Width; y+=meshSize) {
-                    RenderHeightmap(x,x+meshSize, y, y+meshSize);
-                }
-            }
-        }
+        private void RenderTexture() {
+            var width = World.Width;
+            var height = World.Height;
+            texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
 
-        //chatgpt...
-        private void RenderHeightmap(int startX, int endX, int startY, int endY)
-        {
-            // Debug.Log($"Render {startX} {endX} {startY} {endY}");
-            Debug.Assert(startX < endX);
-            Debug.Assert(startY < endY);
-            endX = (int)Mathf.Min(endX, World.Width);
-            endY = (int)Mathf.Min(endY, World.Height);
-            var width = endX-startX;
-            var height = endY-startY;
             var minHeight = World.MinHeight;
             var maxHeight = World.MaxHeight;
 
-            var mesh = new Mesh();
-            var vertices = new Vector3[width * height];
-            var colors = new Color[width * height];
-            var normals = new Vector3[width * height];
-            var triangles = new int[(width - 1) * (height - 1) * 6];
-            var triangleIndex = 0;
-            
-            var worldX = startX;
-            var worldY = startY;
-            for (int meshY = 0; meshY < height; meshY++)
-            {
-                for (int meshX = 0; meshX < width; meshX++)
-                {
-                    var index = meshY * width + meshX;
-                    if(index >= vertices.Length) {
-                        Debug.Log($"{index} {meshX} {meshY} {width} {height}");
-                    }
-                    var heightValue = Mathf.InverseLerp(minHeight, maxHeight, World.GetHeightAt(worldX, worldY));
-                    vertices[index] = new Vector3(worldX, worldY, 0);
-                    normals[index] = new Vector3(0, 0, 1);
-                    colors[index] = colorGradient.Evaluate(heightValue);
-
-                    // Add triangles if not at the border
-                    if (meshX < width - 1 && meshY < height - 1)
-                    {
-                        triangles[triangleIndex++] = index;
-                        triangles[triangleIndex++] = index + width;
-                        triangles[triangleIndex++] = index + width + 1;
-
-                        triangles[triangleIndex++] = index;
-                        triangles[triangleIndex++] = index + width + 1;
-                        triangles[triangleIndex++] = index + 1;
-                    }
-                    worldX++;
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    float heightValue = Mathf.InverseLerp(minHeight, maxHeight, World.GetHeightAt(x, y));
+                    Color color = colorGradient.Evaluate(heightValue);
+                    texture.SetPixel(x, y, color);
                 }
-                worldX = startX;
-                worldY++;
             }
+            texture.Apply();
 
-            mesh.vertices = vertices;
-            mesh.colors = colors;
-            mesh.triangles = triangles;
-            mesh.normals = normals;
+            var quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            quad.transform.SetParent(transform);
+            quad.transform.localPosition = new Vector3(width / 2.0f, height / 2.0f, 0);
+            quad.transform.localScale = new Vector3(width, height, 1);
 
-
-            var terrainObject = new GameObject("Terrain") {
-                transform = {
-                    position = Vector3.zero,
-                    parent = transform
-                }
-            };
-            terrainObject.AddComponent<MeshFilter>().mesh = mesh;
-            terrainObject.AddComponent<MeshRenderer>().material = material;
-;
+            var renderer = quad.GetComponent<Renderer>();
+            renderer.material = material;
+            renderer.material.mainTexture = texture;
         }
     }
-
 }
