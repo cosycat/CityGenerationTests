fbf5c5b2f2cbb85ecefcd1fd3cdf37feeb9ba8b5
Flavia Brogle <flavia.brogle@fhnw.ch>
2024-07-04 00:11:23 +0200

    Set correct position and rotation received from the simulation

--- AgentTileBasedCityGeneration/Assets/Scripts/Simulation/SimulationManager.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/Simulation/SimulationManager.cs
@@ -10,12 +10,22 @@ namespace Simulation {
     public class SimulationManager : MonoBehaviour {
         
         private readonly Dictionary<string, Vehicle> vehicles = new();
+        private GameObject vehicleParent = null!;
         
-        [SerializeField] private Vehicle? vehiclePrefab;
+        [SerializeField] private Vehicle vehiclePrefab = null!;
 
         private IWorld? world;
-        
+
+        private void Awake() {
+            if (vehiclePrefab == null) {
+                Debug.LogError("Vehicle prefab not set.");
+                vehiclePrefab = GameObject.CreatePrimitive(PrimitiveType.Cube).AddComponent<Vehicle>();
+                vehiclePrefab.gameObject.SetActive(false);
+                vehiclePrefab.name = "VehicleDebugPrefab";
             }
+
+            vehicleParent = new GameObject("Vehicles");
+            // vehicleParent.transform.parent = transform;
         }
 
         public void StartSimulation() {
@@ -34,16 +44,19 @@ namespace Simulation {
             
             Debug.Log($"Received {e.VehicleInfo.Length} vehicle data.");
             foreach (var vehicleInfo in e.VehicleInfo) {
-                var position2D = new Vector2(vehicleInfo.x, vehicleInfo.y);
+                var position2D = new Vector2(vehicleInfo.positionX / Constants.METERS_PER_UNIT, vehicleInfo.positionY / Constants.METERS_PER_UNIT);
+                var id = vehicleInfo.id;
                 var worldHeight = world!.GetHeightAt(position2D.x, position2D.y);
-                if (vehicles.TryGetValue(vehicleInfo.id, out var vehicle)) {
-                    vehicle.transform.position = new Vector3(vehicleInfo.x, worldHeight, vehicleInfo.y);
+                if (!vehicles.TryGetValue(id, out var vehicle)) {
+                    vehicle = Instantiate(vehiclePrefab, new Vector3(position2D.x, worldHeight, position2D.y), Quaternion.identity);
+                    vehicle.transform.parent = vehicleParent.transform;
+                    vehicle.ID = id;
+                    vehicles.Add(id, vehicle);
                 }
-                else {
-                    var newVehicle = Instantiate(vehiclePrefab, new Vector3(vehicleInfo.x, worldHeight, vehicleInfo.y), Quaternion.identity);
-                    newVehicle!.ID = vehicleInfo.id;
-                    vehicles.Add(vehicleInfo.id, newVehicle);
-                }
+                vehicle.transform.position = new Vector3(position2D.x, worldHeight, position2D.y);
+                vehicle.transform.rotation = Quaternion.Euler(0, vehicleInfo.rotation + 180, 0);
+                
+                
             }
         }
 
