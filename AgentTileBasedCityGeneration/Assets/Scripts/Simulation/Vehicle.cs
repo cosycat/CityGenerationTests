dc79f5b88012a249a52468249541654764dabd12
Flavia Brogle <flavia.brogle@fhnw.ch>
2024-07-15 17:04:33 +0200

    Add settings to turn vehicle signals on or off

--- AgentTileBasedCityGeneration/Assets/Scripts/Simulation/Vehicle.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/Simulation/Vehicle.cs
@@ -1,24 +1,48 @@
+#nullable enable
+using System;
+using FreeFormGraph;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Utils;
 
 namespace Simulation {
     public class Vehicle : MonoBehaviour {
         
-        [SerializeField] private VehicleSignal blinkerRightGO = null!;
-        [SerializeField] private VehicleSignal blinkerLeftGO = null!;
-        [SerializeField] private VehicleSignal brakeLightGO = null!;
+        [FormerlySerializedAs("blinkerRightGO")] [SerializeField] private VehicleSignal? blinkerRight;
+        [FormerlySerializedAs("blinkerLeftGO")] [SerializeField] private VehicleSignal? blinkerLeft;
+        [FormerlySerializedAs("brakeLightGO")] [SerializeField] private VehicleSignal? brakeLight;
         
-        public string ID { get; internal set; }
-        
+        public string ID { get; internal set; } = null!;
+
+        private void Start() {
+            if (!Settings.UseVehicleSignals) {
+                if (blinkerRight != null) Destroy(blinkerRight.gameObject);
+                if (blinkerLeft != null) Destroy(blinkerLeft.gameObject);
+                if (brakeLight != null) Destroy(brakeLight.gameObject);
+            }
+            else {
+                if (blinkerRight == null || blinkerLeft == null || brakeLight == null) {
+                    Debug.LogError("Vehicle is set to use signals but has missing signal GameObjects!");
+                }
+            }
+            
+            if (ID is null or "") {
+                Debug.LogError("Vehicle ID not set.");
+            }
+            
+            
+        }
+
         public void UpdatePosition(Vector3 position, Quaternion rotation) {
             transform.position = position;
             transform.rotation = rotation;
         }
 
-        public void UpdateSignals(bool blinkerRight, bool blinkerLeft, bool brakeLight) {
-            this.blinkerRightGO.SetSignal(blinkerRight);
-            this.blinkerLeftGO.SetSignal(blinkerLeft);
-            this.brakeLightGO.SetSignal(brakeLight);
+        public void UpdateSignals(bool isBlinkerRightOn, bool isBlinkerLeftOn, bool isBrakeLightOn) {
+            if (!Settings.UseVehicleSignals) return;
+            blinkerRight?.SetSignal(isBlinkerRightOn);
+            blinkerLeft?.SetSignal(isBlinkerLeftOn);
+            brakeLight?.SetSignal(isBrakeLightOn);
         }
     }
 }
