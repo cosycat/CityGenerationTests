8973067b7aeeb75d12bf99862607c9093e710128
Flavia Brogle <flavia.brogle@fhnw.ch>
2024-08-01 22:03:14 -0400

    Fix possible nullpointer error in pathfinding

--- AgentTileBasedCityGeneration/Assets/Scripts/FreeFormGraph/Agents/Pathfinding.cs
+++ AgentTileBasedCityGeneration/Assets/Scripts/FreeFormGraph/Agents/Pathfinding.cs
@@ -204,7 +204,7 @@ namespace FreeFormGraph.Agents {
 
                 Debug.Assert(lastNode != null/*, "Last node is null"*/);
                 var edgeCreated = streetGraph.CreateEdge(lastNode!, wp.Pos, out var newEdge, out lastNode, out var isToNodeNew, out var isEdgeNew);
-                newEdge.Type = p.roadType;
+                if (edgeCreated) newEdge.Type = p.roadType;
                 //edge creation might fail because the road angle is to small or there are too many connections to a node already...
                 //the easiest way to handle these issues is to just remove the road altogether.
                 if(isToNodeNew) removeNodes.Add(lastNode);
